4dd6fc14f5e833a06418a28e4d04d7c114e77a8d
Samuel Vitor Gomes de Carvalho <samuelvitor124@gmail.com>
2023-08-04 19:30:18 -0300

    Repetições

---
+++ modulo E - Repetições/teste.js
@@ -0,0 +1,20 @@
+var c = 1
+var b = 1
+
+while (c < 6) {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
+do {
+    console.log(`Passo b ${b}`)
+    b++
+}while (b < 6)
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?') 
+*/
