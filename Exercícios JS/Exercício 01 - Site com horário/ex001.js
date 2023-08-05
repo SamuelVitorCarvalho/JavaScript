865f8412fe665ccb89f9b0701ab678226da81b57
Samuel Vitor Gomes de Carvalho <samuelvitor124@gmail.com>
2023-08-05 18:34:49 -0300

    Site Finalizado

--- Exercícios JS/Exercício 01 - Site com horário/ex001.js
+++ Exercícios JS/Exercício 01 - Site com horário/ex001.js
@@ -1,17 +1,21 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
+    var edu = window.document.getElementById('edu')
     var data = new Date()    
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         img.src = 'imagens/manha.png'
         document.body.style.background = '#ca8a68'
+        edu.innerHTML = 'Bom dia!'
     } else if (hora >= 12 && hora <= 18) {
         img.src = 'imagens/tarde.png'
         document.body.style.background = '#74495c'
+        edu.innerHTML = 'Boa tarde!'
     } else {
         img.src = 'imagens/noite.png'
         document.body.style.background = '#1a3656'
+        edu.innerHTML = 'Boa noite!'
     }
 }
