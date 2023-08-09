c3fccd1e639e084b6b06d5212c161d4cd87664d8
Samuel Vitor Gomes de Carvalho <samuelvitor124@gmail.com>
2023-08-09 20:17:33 -0300

    Estudo sobre Vetores

---
+++ modulo F - JS avançado/ex015.js
@@ -0,0 +1,22 @@
+let num = [5, 8, 2, 9, 3]
+num[2] = 4
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é o ${num[0]}`)
+
+//Para mostrar todos os valores do vetor
+
+for(var pos = 0; pos < num.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+//Um "for" feito para array:
+
+for(let pos in num) {
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+var ind = num.indexOf(5)
+console.log(`O valor está na posição ${ind}`)
