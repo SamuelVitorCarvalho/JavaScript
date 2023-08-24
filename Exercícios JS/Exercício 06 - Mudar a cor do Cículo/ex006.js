6113c4df462019b4110ba83405f01d9a33293a01
Samuel Vitor Gomes de Carvalho <samuelvitor124@gmail.com>
2023-08-24 19:51:57 -0300

    Finalizado

--- Exercícios JS/Exercício 06 - Mudar a cor do Cículo/ex006.js
+++ Exercícios JS/Exercício 06 - Mudar a cor do Cículo/ex006.js
@@ -1,17 +1,20 @@
 let text = document.getElementById('text')
 let button = document.getElementById('button')
 let circle = document.getElementById('circle')
+let lowercase = text.value.toLowerCase(text)
 
 function changeColor() {
     if (text.value.length == 0) {
         window.alert('Please provide a color name!')
-    } else if (text.value == 'Blue') {
+    } else if (text.value == 'Blue' || text.value == 'blue') {
         document.getElementById('circle').style.background = 'blue'
-    } else if (text.value == 'Red') {
+    } else if (text.value == 'Red' || text.value == 'red') {
         document.getElementById('circle').style.background = 'red'
-    } else if (text.value == 'Orange') {
+    } else if (text.value == 'Orange' || text.value == 'orange') {
         document.getElementById('circle').style.background = 'orange'
-    } else if (text.value == 'White') {
+    } else if (text.value == 'White' || text.value == 'white') {
         document.getElementById('circle').style.background = 'white'
     }
+    text.value = ''
+    text.focus()
 }
