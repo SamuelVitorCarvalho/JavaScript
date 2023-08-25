044bad640ed5a7ccd8d98b5f3ab68010c7fc94aa
Samuel Vitor Gomes de Carvalho <samuelvitor124@gmail.com>
2023-08-24 21:09:51 -0300

    Tentando fazer com objetos

--- Exercícios JS/Exercício 06 - Mudar a cor do Cículo/ex006.js
+++ Exercícios JS/Exercício 06 - Mudar a cor do Cículo/ex006.js
@@ -1,20 +1,39 @@
 let text = document.getElementById('text')
 let button = document.getElementById('button')
 let circle = document.getElementById('circle')
-let lowercase = text.value.toLowerCase(text)
 
+/*
 function changeColor() {
+    let color = text.value.toLowerCase()
     if (text.value.length == 0) {
         window.alert('Please provide a color name!')
-    } else if (text.value == 'Blue' || text.value == 'blue') {
+    } else if (color == 'blue') {
         document.getElementById('circle').style.background = 'blue'
-    } else if (text.value == 'Red' || text.value == 'red') {
+    } else if (color == 'red') {
         document.getElementById('circle').style.background = 'red'
-    } else if (text.value == 'Orange' || text.value == 'orange') {
+    } else if (color == 'orange') {
         document.getElementById('circle').style.background = 'orange'
-    } else if (text.value == 'White' || text.value == 'white') {
+    } else if (color == 'white') {
         document.getElementById('circle').style.background = 'white'
     }
     text.value = ''
     text.focus()
+}
+*/
+
+const colors = {
+    color1: "blue",
+    color2: "red",
+    color3: "orange",
+    color4: "white"
+}
+
+function changeColor() {
+    if (text.value.length == 0) {
+        window.alert('Please provide a color name!')
+    } else if (Object.values(colors).includes(text)) {
+        document.getElementById('circle').style.background = text
+    } else {
+        window.alert('Color invalid!')
+    }
 }
