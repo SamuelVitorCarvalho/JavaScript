f38f4623f00e9826d3e9bede1523be3e9583138d
Samuel Vitor Gomes de Carvalho <samuelvitor124@gmail.com>
2023-08-28 20:44:33 -0300

    Comecei o EX007

---
+++ Exercícios JS/Exercício 07 - Lista de tarefas/ex007.js
@@ -0,0 +1,12 @@
+const res = document.getElementById('res')
+
+var vetor = {
+    task1: 'Clean House',
+    task2: 'Clean PC',
+    task3: 'Washe dishes',
+}
+
+function adicionar() {
+    var texto = '1'
+    res.textContent = texto
+}
