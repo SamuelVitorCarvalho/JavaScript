c9826fe6c605689eabdd4042d76fa3a4714ab08f
Samuel Vitor Gomes de Carvalho <samuelvitor124@gmail.com>
2023-09-05 21:45:18 -0300

    Script FINALIZADO!!

--- Exercícios JS/Exercício 08 - Lista para editar/newCode.js
+++ Exercícios JS/Exercício 08 - Lista para editar/newCode.js
@@ -9,20 +9,32 @@ var itens = []
 function adicionar() {
     let newTask = inputTask.value
     itens.push({task: newTask})
-    res.innerHTML += `<div id="newTask">
-        <input type="text" name="tarefa" id="taskName" value="${newTask}" readonly>
-        <div id="deledit">
-            <div id="editTask" onclick="editTask(this)">
-                <span class="material-symbols-outlined">edit</span>
-            </div>
-            <div id="deleteTask" onclick="deleteTask(this)">
-                <span class="material-symbols-outlined">delete</span>
-            </div>
-        </div>
-    </div>`
+
+    //chama a função 
+    atualizar()
 
     inputTask.value = ''
     inputTask.focus()
+}
+
+function atualizar () { // Serve para criar os "res" a aprtir do itens[]
+    // Esvaziar todo o conteudo da section#res
+    res.innerHTML = ''
+
+    // Criar um input para cada objeto que está em itens (por meio de um for)
+    for (i in itens) {
+        res.innerHTML += `<div id="newTask${i}" class="newTask">
+            <input type="text" name="tarefa" id="taskName" value="${itens[i].task}" readonly>
+            <div id="deledit">
+                <div id="editTask" onclick="editTask(this)">
+                    <span class="material-symbols-outlined">edit</span>
+                </div>
+                <div id="deleteTask" onclick="deleteTask(this)">
+                    <span class="material-symbols-outlined">delete</span>
+                </div>
+            </div>
+        </div>`
+    }
 }
 
 // Função pra editar a tarefa
@@ -39,14 +51,35 @@ function editTask(ed) {
         newInput.readOnly = true
         saveBut.textContent = 'edit'
         let newName = newInput.value
-        console.log(newName) // atualizar o itens[]
+        let index = ed.parentElement.parentElement.id //pega o "id" do elemento inteiro
+        let num = index.replace(/[^0-9]/g, '')
+        itens[num].task = newName
+        atualizar()
     }
 }
 
 // Função para deletar a tarefa
 
 function deleteTask(del) {
-    del.parentElement.parentElement.remove()
+    let index = del.parentElement.parentElement.id //pega o "id" do elemento inteiro
+    let num = index.replace(/[^0-9]/g, '') // aqui pega so o numero do id, ou seja,a posição q ele ocupa no itens[]
+    itens.splice(num, 1) // aqui exclui o elemento do objeto
+    atualizar()
 }
 
-// Preciso atualizar o novo valor da tarefa no "itens[]", preciso mudar o CSS do input de edição (deixar ele transparente e sem o cursor),
+/*
+var vetor = [
+    {
+    vet: [0,1,2,3,4]
+},
+{
+    task: '1',
+    n: [{
+        nu: 'r'
+    }]
+}
+
+]
+
+console.log(vetor[1].n[0].nu)
+*/
